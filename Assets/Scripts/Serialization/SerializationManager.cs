b886653a79af2c1cb1deff792f1cef89112cf79d
Santiago henry <henrysantiago95@gmail.com>
2023-09-25 15:36:06 -0300

    Serialization system core

---
+++ Assets/Scripts/Serialization/SerializationManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace SandWar.Serialization
+{
+    public class SerializationManager : MonoBehaviour
+    {
+        private const string SAVE_DATA_KEY = "SandWarSaveData";
+
+        public static void SaveData<T>(string key, T data)
+        {
+            Dictionary<string, object> saveData = LoadSaveData();
+
+            if (saveData.ContainsKey(key))
+            {
+                saveData[key] = data;
+            }
+            else
+            {
+                saveData.Add(key, data);
+            }
+
+            SaveToFile(saveData);
+        }
+
+        public static bool TryLoadData<T>(string key, out T data)
+        {
+            Dictionary<string, object> saveData = LoadSaveData();
+
+            if (saveData.ContainsKey(key))
+            {
+                data = (T)saveData[key];
+                return true;
+            }
+
+            data = default;
+            return false;
+        }
+
+        private static void SaveToFile(Dictionary<string, object> saveData)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream fileStream = new FileStream(GetSaveFilePath(), FileMode.Create);
+            formatter.Serialize(fileStream, saveData);
+            fileStream.Close();
+        }
+
+        private static string GetSaveFilePath()
+        {
+            return Path.Combine(Application.persistentDataPath, SAVE_DATA_KEY + ".dat");
+        }
+
+        private static Dictionary<string, object> LoadSaveData()
+        {
+            if (File.Exists(GetSaveFilePath()))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream fileStream = new FileStream(GetSaveFilePath(), FileMode.Open);
+                Dictionary<string, object> saveData = formatter.Deserialize(fileStream) as Dictionary<string, object>;
+                fileStream.Close();
+                return saveData;
+            }
+            else
+            {
+                return new Dictionary<string, object>();
+            }
+        }
+
+    }
+}
+
