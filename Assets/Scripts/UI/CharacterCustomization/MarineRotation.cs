640cc1af9d4de9b932f159bfffd629e9e3e56dc5
AgustinRambaldo <73607427+AgusRambal@users.noreply.github.com>
2023-10-05 16:28:59 -0300

    Marine rotation and scene managment

---
+++ Assets/Scripts/UI/CharacterCustomization/MarineRotation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarineRotation : MonoBehaviour 
+{
+    private Transform marineTransform;
+    private Vector3 lastMousePosition;
+    
+    private void Awake() {
+        marineTransform = transform;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(1)) {
+            Vector3 mouseDelta = lastMousePosition - Input.mousePosition;
+            
+            // Fix huge jumps when Unity loses focus
+            //mouseDelta.y = Mathf.Clamp(mouseDelta.y, -200, +200);
+            mouseDelta.x = Mathf.Clamp(mouseDelta.x, -200, +200);
+
+            float rotateSpeed = .2f;
+
+            marineTransform.localEulerAngles += new Vector3(0f, mouseDelta.x, 0f) * rotateSpeed;
+
+            float rotationXMin = -7f;
+            float rotationXMax = +10f;
+
+            float localEulerAnglesX = marineTransform.localEulerAngles.x;
+            if (localEulerAnglesX > 180) {
+                localEulerAnglesX -= 360f;
+            }
+            float rotationX = Mathf.Clamp(localEulerAnglesX, rotationXMin, rotationXMax);
+
+            marineTransform.localEulerAngles = new Vector3(rotationX, marineTransform.localEulerAngles.y, marineTransform.localEulerAngles.z);
+        }
+        
+        lastMousePosition = Input.mousePosition;
+    }
+}
